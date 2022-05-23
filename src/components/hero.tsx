b1d48a4e9602cf1bd1bd9b1c776b4e563f04d86b
Maciej Błędkowski <pub@mble.dk>
2022-05-23 15:37:47 +0200

    Added GitHub and Documentation buttons

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -22,7 +22,7 @@ const HeroBox = styled.div`
     border-radius: 2vw;
   }
 
-  > h1 {
+  > div > h1 {
     margin: 2vw;
     width: min-content;
   }
@@ -35,6 +35,34 @@ const HeroBox = styled.div`
   }
 `
 
+const StyledA = styled.a`
+  background: #0d30d3;
+  border: none;
+  border-radius: clamp(.4rem, .8vw, .8rem);
+  padding: clamp(.5rem, 1vw, 1rem);
+  margin: clamp(.5rem, 1vw, 1rem);
+  font-size: clamp(1rem, 2vw, 2rem);
+  font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+  transition: box-shadow 0.2s ease-in-out;
+  box-shadow: 0 3px 12px 1px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+  font-family: "Lato", sans-serif;
+  font-weight: bold;
+  @media (max-width: 800px) {
+    font-size: 1.5rem;
+    padding: 1rem;
+    margin: 1rem;
+    border-radius: .8rem;
+
+  }
+  :hover {
+    box-shadow: 0 3px 12px 1px rgba(0, 0, 0, 0.5);
+  }
+`
+
+
 export default function Hero() {
   return (
     <HeroBox>
@@ -43,7 +71,11 @@ export default function Hero() {
         <script id="asciicast-335480" src="https://asciinema.org/a/335480.js" async data-autoplay="1" data-preload="1"
           data-loop="1" data-columns="70" data-rows="35"></script>
       </div>
-      <h1>Minecraft Package Manager</h1>
+      <div>
+        <h1>Minecraft Package Manager</h1>
+        <StyledA className="github" href="https://github.com/mbledkowski/mpm">GitHub</StyledA>
+        <StyledA className="documentation" href="https://github.com/mbledkowski/mpm/wiki">Documentation</StyledA>
+      </div>
     </HeroBox>
   )
 }
